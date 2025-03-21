15b429029d44ac4e3d387a3fdec79aa0001453ef
Kshitij Kochhar <kshitij@ctfguide.com>
2025-03-21 19:13:06 -0400

    [feat] proj init

---
+++ src/main/java/com/kshitijkochhar/weathersmtp/WeatherSmtpApplication.java
@@ -0,0 +1,13 @@
+package com.kshitijkochhar.weathersmtp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WeatherSmtpApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WeatherSmtpApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/kshitijkochhar/weathersmtp/WeatherSmtpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kshitijkochhar.weathersmtp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WeatherSmtpApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
